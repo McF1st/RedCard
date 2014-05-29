1e94e99b65431606b90457ae05fb7c735692c02d
toftan <emma.toft@gmail.com>
2014-05-29 18:05:34 +0200

    Created Android project

---
+++ android/src/com/et/redcard/RedCardActivity.java
@@ -0,0 +1,17 @@
+package com.et.redcard;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class RedCardActivity extends Activity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_red_card);
+	}
+}
